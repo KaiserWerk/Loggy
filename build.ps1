67684d481a348cb73dff353aa2076e9f2fe901de
Robin Kaiser <web@r-k.mx>
2022-02-03 22:11:54 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,12 +1,14 @@
 $sourcecode = ".\main.go"
 $target = "build\loggy"
 $version = "1.0.0"
+$commit = git rev-parse HEAD
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
-# Windows, 64-bit
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-win64.exe" -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Date=$($date)'" $sourcecode
+
 # Linux, 64-bit
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-linux64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-$($version)-linux-amd64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
 # Raspberry Pi
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=5; go build -o "$($target)-raspi32"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=7; go build -o "$($target)-$($version)-linux-arm7"    -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
 # macOS
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-macos64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-$($version)-macos-amd64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
+# Windows, 64-bit
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-$($version)-win-amd64.exe" -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
