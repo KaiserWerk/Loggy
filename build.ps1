20b785ac8db3636a838cf322f88029643e51660e
Robin Kaiser <web@r-k.mx>
2022-02-03 22:15:47 +0100

    file name fix

--- build.ps1
+++ build.ps1
@@ -5,10 +5,10 @@ $commit = git rev-parse HEAD
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
 
 # Linux, 64-bit
-$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-$($version)-linux-amd64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'amd64';             go build -o "$($target)-v$($version)-linux-amd64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
 # Raspberry Pi
-$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=7; go build -o "$($target)-$($version)-linux-arm7"    -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'linux';   $env:GOARCH = 'arm'; $env:GOARM=7; go build -o "$($target)-v$($version)-linux-arm7"    -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
 # macOS
-$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-$($version)-macos-amd64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'darwin';  $env:GOARCH = 'amd64';             go build -o "$($target)-v$($version)-macos-amd64"   -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
 # Windows, 64-bit
-$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-$($version)-win-amd64.exe" -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
+$env:GOOS = 'windows'; $env:GOARCH = 'amd64';             go build -o "$($target)-v$($version)-win-amd64.exe" -ldflags "-s -w -X 'main.Version=$($version)' -X 'main.Commit=$($commit)' -X 'main.Date=$($date)'" $sourcecode
